f2faa08b8041a23cd770e64870673eaf5ce06eab
reaniwen <reaniwen@gmail.com>
2015-10-17 23:52:42 -0400

    r0.1.4.1
    add class of fish and tools

---
+++ Zone'in/Zone'in/FishATools.swift
@@ -0,0 +1,33 @@
+//
+//  FishAPrize.swift
+//  Zone'in
+//
+//  Created by Rean on 10/17/15.
+//  Copyright © 2015 Rean. All rights reserved.
+//
+
+import Foundation
+
+class Fish{
+    var name:String = ""
+    var imageString = ""
+    
+    init(fishName:String, imageString:String){
+        self.name = fishName
+        self.imageString = imageString
+    }
+    
+}
+
+class FishTools{
+    var toolName:String = ""
+    var toolPrice:Int = 0
+    var toolImageStr:String = ""
+    
+    init(toolName:String, toolPrice:Int, toolImage:String){
+        self.toolName = toolName
+        self.toolPrice = toolPrice
+        self.toolImageStr = String
+    }
+    
+}

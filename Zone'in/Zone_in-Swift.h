1be34a12c391127bf48dd441a68de6c276e23378
reaniwen <reaniwen@gmail.com>
2015-10-24 13:55:48 -0400

    strange update
    when I added a manual zone_in-Swift.h to try to change the automatic
    file in the right half, before I copy the code into the manual file, it
    changed.

---
+++ Zone'in/Zone_in-Swift.h
@@ -0,0 +1,13 @@
+//
+//  Zone_in-Swift.h
+//  Zone'in
+//
+//  Created by Rean on 10/24/15.
+//  Copyright © 2015 Rean. All rights reserved.
+//
+
+#ifndef Zone_in_Swift_h
+#define Zone_in_Swift_h
+
+
+#endif /* Zone_in_Swift_h */
